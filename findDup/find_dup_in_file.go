9ef2f108139ff4c5a94576dd14b92f69dc3300ba
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2017-07-14 18:15:11 +0900

    find dupe

---
+++ findDup/find_dup_in_file.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func countLines(f *os.File, counts map[string]int) {
+    input := bufio.NewScanner(f)
+    for input.Scan() {
+        counts[input.Text()]++
+    }
+}
+
+func main() {
+    counts := make(map[string]int)
+    files := os.Args[1:]
+    fmt.Printf("os.Args[0]=%s\n", os.Args[0])
+    if len(files) == 0 {
+        countLines(os.Stdin, counts)
+    } else {
+        for _, arg := range files {
+            f, err := os.Open(arg)
+            if err != nil { //if ones fail to open file
+                fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+                continue
+            }
+            countLines(f, counts)
+            f.Close()
+        }
+    }
+
+    for line, n := range counts {
+        if n > 1 {
+            fmt.Printf("%d\t%s\n", n, line)
+        }
+    }
+}
