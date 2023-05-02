c9a88b490e5d4cd3948f5af6feac98445de34bba
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2023-05-02 22:33:09 +0900

    interface

---
+++ interfaces/interfaces.go
@@ -0,0 +1,61 @@
+package main 
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+// The implementation for rects
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+// The implementation for rects
+func (r rect) perim() float64 {
+	return 2 * r.width + 2 * r.height
+}
+
+// The implementation for circles
+func (c circle) area() float64 {
+	return c.radius
+}
+// The implementation of circles
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func main(){
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
+
+	measure(r)
+	measure(c)
+}
+
+
+
+
+
+
+
+
+
